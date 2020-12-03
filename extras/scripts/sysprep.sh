b6109afa867c09783b682e2eb65e9f3ca180f1d7
Michael de Gans <47511965+mdegans@users.noreply.github.com>
2020-12-03 11:11:33 -0800

    Fix sysprep (#9)
    * add const to easily change cuda version
    * Add python3-dev to installed packages
    (relates to #7) -  a missing requirement for pucuda
    * better cuda post-install setup
    * add opencv install to sysprep.sh (temporarily disableD)
    (relates to #7) - missing opencv
    * better `/etc/skel/.profile` patching

--- extras/scripts/sysprep.sh
+++ extras/scripts/sysprep.sh
@@ -1,10 +1,29 @@
 #!/bin/bash
 
+set -e
+
 # This is a sample script to be run with chroot.py (or otherwise in a chroot)
 # to remove and install some apt packages from the rootfs (before first boot)
-# You can use a script like this to prepare a system for first run.
+# You can use a script like this to prepare a system for first run. It:
 
-set -e
+# * Installs CUDA, creates /usr/local/cuda symlink, and modifes
+# `/etc/skel/.profile` to add CUDA_HOME and nvcc to path for all new users
+
+# * Installs TensorRT, nvidia-docker, and some python3 development packages
+
+# * Builds and installs CUDA enhanced OpenCV
+
+# change these if cuda version / apt version changes
+readonly CUDA_VER="10.2"
+readonly OPENCV_VER="4.5.0"
+
+# these probably shouldn't change
+readonly CUDA_HOME_REAL="/usr/local/cuda-${CUDA_VER}"
+readonly CUDA_HOME="/usr/local/cuda"
+readonly CUDA_VER_APT=${CUDA_VER//[.]/-}
+readonly PATH=${CUDA_HOME}/bin:${PATH}
+
+echo "installing apt dependencies"
 
 # purge useless packages and configuration
 apt-get purge -y \
@@ -20,10 +39,34 @@ apt-get dist-upgrade -y
 
 # install packages, cleaning up any no longer needed from above
 apt-get install -y --autoremove --no-install-recommends \
-  cuda-compiler-10-2 \
-  cuda-libraries-dev-10-2 \
+  cuda-compiler-${CUDA_VER_APT} \
+  cuda-libraries-dev-${CUDA_VER_APT} \
   nano \
   nvidia-container \
   nvidia-tensorrt \
+  python3-dev \
   python3-wheel \
 && rm -rf /var/lib/apt/lists/*
+
+echo "doing cuda post-install setup"
+# make /usr/local/cuda link
+ln -s $CUDA_HOME_REAL $CUDA_HOME
+# patch /etc/skel/.profile
+cat >> /etc/skel/.profile <<EOL
+
+# set the cuda home appropriately
+export CUDA_HOME=${CUDA_HOME}
+# add the cuda bindir so nvcc and other tools are in $PATH
+PATH=\${CUDA_HOME}/bin:\${PATH}
+EOL
+
+# build and install OpenCV
+# this works, but is very slow. Disabling until native cross compilation support
+# is added to chroot.py
+
+# cd /tmp
+# git clone https://github.com/mdegans/nano_build_opencv.git
+# cd nano_build_opencv
+# ./build_opencv.sh $OPENCV_VER
+# cd ..
+# rm -rf nano_build_opencv
